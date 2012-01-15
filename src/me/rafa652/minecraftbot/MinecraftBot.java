b4e60a72f78d8bd9330575bc0c7c3d9d05158d56
Rafael Ruelas <ruelas.rafael@gmail.com>
2012-01-15 15:51:28 -0800

    Small fix with permissions

--- src/me/rafa652/minecraftbot/MinecraftBot.java
+++ src/me/rafa652/minecraftbot/MinecraftBot.java
@@ -12,10 +12,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jibble.pircbot.User;
 
 public class MinecraftBot extends JavaPlugin {
-	private String version = "0.95";
+	private String version = "0.96";
 	public Logger log = Logger.getLogger("Minecraft");
 	
 	public MinecraftBotConfiguration config;
@@ -99,23 +98,28 @@ public class MinecraftBot extends JavaPlugin {
 			boolean showusage = (args.length == 1);
 			String subcommand = args[0].toLowerCase();
 			
-			if (subcommand.equals("op") && permitted(sender, "op")) {
+			if (subcommand.equals("op")) {
+				if (!permitted(sender, "op")) return true;
 				if (showusage) sender.sendMessage("/op nick");
 				else bot.op(config.bot_channel, args[1]);
 			}
-			else if (subcommand.equals("deop") && permitted(sender, "op")) {
+			else if (subcommand.equals("deop")) {
+				if (!permitted(sender, "op")) return true;
 				if (showusage) sender.sendMessage("/deop nick");
 				else bot.deOp(config.bot_channel, args[1]);
 			}
-			else if (subcommand.equals("voice") && permitted(sender, "voice")) {
+			else if (subcommand.equals("voice")) {
+				if (!permitted(sender, "voice")) return true;
 				if (showusage) sender.sendMessage("/voice nick");
 				else bot.voice(config.bot_channel, args[1]);
 			}
-			else if (subcommand.equals("devoice") && permitted(sender, "voice")) {
+			else if (subcommand.equals("devoice")) {
+				if (!permitted(sender, "voice")) return true;
 				if (showusage) sender.sendMessage("/devoice nick");
 				else bot.deVoice(config.bot_channel, args[1]);
 			}
-			else if (subcommand.equals("kick") && permitted(sender, "kick")) {
+			else if (subcommand.equals("kick")) {
+				if (!permitted(sender, "kick")) return true;
 				if (showusage) sender.sendMessage("/kick nick [reason]");
 				else {
 					String reason = "";
@@ -124,31 +128,17 @@ public class MinecraftBot extends JavaPlugin {
 					bot.kick(config.bot_channel, args[1], reason);
 				}
 			}
-			/* Thought of adding an /irc ban command that would take a user as
-			 * an argument then ban that user, but the bot needs a hostmask to ban.
-			 * Can't get a user's hostmask.
-			else if (subcommand.equals("ban") && permitted(sender, "ban")) {
-				// Must find the user's hostmask to be able to ban
-				boolean found = false;
-				String hostmask;
-				
-				User users[] = bot.getUsers(config.bot_channel);
-				for (int i=0;i<users.length;i++) {
-					hostmask = users[i].
-					if (users[i].getNick().equals(args[1])) found = true;
-				}
-				
-				if (found) bot.ban(config.bot_channel, hostmask);
-			}
-			*/
-			else if (subcommand.equals("connect") && permitted(sender, "manage")) {
+			else if (subcommand.equals("connect")) {
+				if (!permitted(sender, "manage")) return true;
 				if (bot.isConnected()) sender.sendMessage("Already connected to IRC!");
 				else bot.connect();
 			}
-			else if (subcommand.equals("rejoin") && permitted(sender, "manage")) {
+			else if (subcommand.equals("rejoin")) {
+				if (!permitted(sender, "manage")) return true;
 				bot.joinChannel();
 			}
-			else if (subcommand.equals("disconnect") && permitted(sender, "manage")) {
+			else if (subcommand.equals("disconnect")) {
+				if (!permitted(sender, "manage")) return true;
 				if (!bot.isConnected()) sender.sendMessage("Already not connected to IRC!");
 				else bot.disconnect();
 			}
@@ -164,7 +154,7 @@ public class MinecraftBot extends JavaPlugin {
 		if (sender instanceof ConsoleCommandSender) return true;
 		boolean p = (sender.hasPermission("minecraftbot." + permission));
 		
-		if (!p) sender.sendMessage(ChatColor.DARK_RED + "You are not permitted to use this command.");
+		if (!p) sender.sendMessage(ChatColor.RED + "You are not permitted to use this command.");
 		return p;
 	}
 }
